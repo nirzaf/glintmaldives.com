6fe60b8927354bc4acff6e09698b0902aa10a3fa
nirzaf <mfmfazrin1986@gmail.com>
2025-02-07 21:27:58 +0300

    🛠️ src/pages/Home.tsx -> Removed Header and Footer imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Features from '../components/Features';
 import Services from '../components/Services';
 import Portfolio from '../components/Portfolio';
-import Footer from '../components/Footer';
 
 const Home: React.FC = () => {
   return (
     <>
-      <Header />
       <Hero />
       <main id="main">
         <About />
@@ -18,7 +15,6 @@ const Home: React.FC = () => {
         <Services />
         <Portfolio />
       </main>
-      <Footer />
     </>
   );
 };
